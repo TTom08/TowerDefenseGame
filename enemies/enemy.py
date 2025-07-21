6ef0b10e4bc44aed79e88883fcf5f652146d88dc
Tóth Tamás <t.tamas3208@gmail.com>
2025-07-21 08:49:30 +0200

    feat: implemented enemy class, made enemy movement

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 class Enemy:
     imgs = []
@@ -10,8 +11,12 @@ class Enemy:
         self.height = height
         self.animation_count = 0
         self.health = 1
-        self.path = []
+        self.path = [(1053, 16), (1056, 61), (1055, 116), (1066, 183), (1050, 254), (1007, 283), (893, 275), (798, 276), (643, 271), (489, 277), (334, 275), (229, 282), (180, 280), (147, 307), (155, 350), (173, 400), (181, 456), (193, 543), (203, 612), (213, 693), (220, 749), (263, 770), (347, 775), (429, 780), (524, 783), (616, 792), (705, 795), (751, 777), (763, 712), (772, 617), (786, 555), (850, 538), (970, 541), (1050, 545), (1105, 551), (1111, 618), (1104, 695), (1114, 754), (1153, 775), (1210, 786), (1257, 787)]
         self.img = None
+        self.vel = 3
+        self.path_pos = 0
+        self.move_count = 0
+        self.move_distance = 0
 
     def draw(self, win):
         self.animation_count += 1
@@ -29,8 +34,29 @@ class Enemy:
                 return True
         return False
 
-    def move(self):
-        pass
+    def move(self, change):
+        x1,y1 = self.path[self.path_pos]
+        if self.path_pos + 1 >=  len(self.path):
+            x2, y2 = (1240,800)
+        else:
+            x2, y2 = self.path[self.path_pos + 1]
+
+        move_distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+        self.move_count += 1
+        dirn = (x2 - x1, y2 - y1)
+
+        move_x, move_y = (self.x + dirn[0] * self.move_count, self.y + dirn[1] * self.move_count)
+        self.dis += math.sqrt((move_x - x1) ** 2 + (move_y - y1) ** 2)
+
+        if self.dis >= move_distance:
+            self.dis = 0
+            self.move_count = 0
+            self.path_pos += 1
+
+        self.x = move_x
+        self.y = move_y
+
 
     def hit(self):
         self.health == 1
--- game.py
+++ game.py
@@ -1,29 +1,48 @@
 import os.path
-
 import pygame
 
 class Game:
     def __init__(self):
-        self.width = 960
-        self.height = 720
-        self.win = pygame.display.set_mode((self.width, self.height))
+        self.width = 1280
+        self.height = 960
+        self.window = pygame.display.set_mode((self.width, self.height))
 
         self.enemies = []
         self.towers = []
         self.lives = 10
         self.money = 200
-        self.background = pygame.image.load(os.path.join("towers", "map.png"))
+        self.background = pygame.transform.scale(
+            pygame.image.load(os.path.join("assets","other", "map.png")),
+            (self.width, self.height)
+        )
+        # self.clicks = []   used for mapping enemy path
 
     def run(self):
         run = True
-
+        clock = pygame.time.Clock()
+        
         while run:
+            clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
+                pos = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # self.clicks.append(pos)      also used for mapping enemy path
+                    # print(self.clicks)
+                    pass
+                    
+            self.draw()
+
         pygame.quit()
 
     def draw(self):
-        self.win.blit(self.background, (0,0))
+        self.window.blit(self.background, (0,0))
+        # for p in self.clicks:
+            # pygame.draw.circle(self.window, (255,0,0), (p[0], p[1]), 5, 0)      used for mapping enemy path
         pygame.display.update()
+
+tower_game = Game()
+tower_game.run()
